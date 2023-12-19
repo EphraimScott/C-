80c55b10574a858fe5c7e84c4b33da865a1ee1c8
renatasousa3 <renatacsousa45@gmail.com>
2023-12-19 20:12:58 -0300

    first c#

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿string? Produto;
+Console.WriteLine("Insira o nome do produto: ");
+Produto = Console.ReadLine();
+
+int Quantidade;
+Console.WriteLine("Insira uma quantidade ao produto: ");
+Quantidade = Convert.ToInt32(Console.ReadLine());
+
+int Valor;
+Console.WriteLine("Insira o valor do produto: ");
+Valor = Convert.ToInt32(Console.ReadLine());
+
+int mult = Quantidade * Valor;
+
+Console.WriteLine("O valor de {0} é {1}", Produto, mult);
